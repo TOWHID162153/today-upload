c5c0109819567656bb892b88337f1d30ec45286c
TOWHID <web.development162253@gmail.com>
2025-02-22 23:39:14 +0600

    5th commit

--- problem03.js
+++ problem03.js
@@ -1,39 +1,35 @@
 // # Problem-03 : Who will Win  ( 🥭Mango /🍌 Banana ) 
 // # Problem 03 solve : 
-function  electionResult(votes) {
+function electionResult(votes) {
     
-    let mango = "mango";
-    let banana = "banana"; 
     let mangoParty = 0;
     let bananaParty = 0;
-    for(let i= 0; 1 < votes.length; i++){
-        if(votes[i] === mango){
+    for(let i= 0; i < votes.length; i++){
+        if( typeof votes[i] === "string" && votes[i] === "mango" ){
             mangoParty++;
-        } else if(votes[i] === banana){
+
+        }
+        else if( typeof votes[i] === "string" && votes[i] === "banana" ){
             bananaParty++;
         }
     }
 
-    if (mangoParty > bananaParty && typeof votes === "array") {
-        return "Mango";
-    }                
-    else if(bananaParty > mangoParty && typeof votes === "array") {
-        return "Banana";
-    }      
-    else if( typeof votes !== "array" && !typeof votes === "array" ) {
+    if(!Array.isArray(votes)){
         return "Invalid";
-    }  
-    else {
-        return "Draw";
+    } else if(mangoParty > bananaParty){
+        return "Mango" ;
+    } else if(bananaParty > mangoParty){
+        return "Bnanan";
+    } else{
+        return "Draw" ;
     }
-
 }
 
 electionResult(["mango", "banana", "mango", "banana", "mango"]); // "Mango"
 electionResult([]); // "Draw"
 electionResult(["mango", "banana", "jaker party" , "no"]); // "Draw"
 electionResult(["mango"]); // "Mango"
-electionResult(["banana", "banana", "age e valo chilam" , "no"]); // "Banana"
+electionResult(["banana", "banana", "age e valo chilam" , "no"]); // // "Banana"
 electionResult({result: "mango , banana ,  mango"}); // "Invalid"
-electionResult("mango , banana"); // "Invalid"
-electionResult(["mango", "BananA", "na vote", "na vote"]); // "Banana"
+electionResult("mango , banana");// "Invalid"
+electionResult(["mango", "BananA", "na vote", "na vote"]); // "Mango"

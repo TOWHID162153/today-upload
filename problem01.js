91b81543aa883b451aa1a013cfab9eacf806ab7b
TOWHID <web.development162253@gmail.com>
2025-02-22 08:52:14 +0600

    2nd commit

--- problem01.js
+++ problem01.js
@@ -1,4 +1,19 @@
-// #Problem 01 solve : 
-function cashOut(){
-    
+// # Problem-01 : Calculate the cash-out Charge 
+// # Problem 01 solve : 
+function cashOut(money){
+    if(money >= 0 && money === Number(+ money)){
+        let chargeAmount = money * 1.75 / 100;
+        return chargeAmount;
+    }
+    else{
+        return "Invalid";
+    } 
 }
+
+cashOut(2000);
+cashOut(100);
+cashOut(999);
+cashOut(-350);
+cashOut(0);
+cashOut("101");
+cashOut("mewauu");

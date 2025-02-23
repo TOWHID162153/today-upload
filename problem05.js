c842b96580d43c9023a366a2d74158823d9922c0
TOWHID <web.development162253@gmail.com>
2025-02-24 03:20:34 +0600

    maybe last commit

--- problem05.js
+++ problem05.js
@@ -25,11 +25,11 @@ function  calculateWatchTime( times ) {
          };
     }
 
-calculateWatchTime([100, 99, 119, 300]) ; // { hour: 0, minute: 10, second: 18 }
-calculateWatchTime([1000, 2000, 725]) ; // { hour: 1, minute: 2, second:5 }
-calculateWatchTime([100, 3800]) ; // { hour: 1, minute: 5, second: 0 }
-calculateWatchTime([]) ; // { hour: 0, minute: 0, second: 0 }
-calculateWatchTime([5600]) ; // { hour: 1, minute: 33, second: 20 }
-calculateWatchTime([100, 3800, "90" ]) ; // "Invalid"
+// console.log(calculateWatchTime([100, 99, 119, 300]));
+// console.log(calculateWatchTime([1000, 2000, 725]));
+// console.log(calculateWatchTime([100, 3800]) );
+// console.log(calculateWatchTime([]));
+// console.log(calculateWatchTime([5600]));
+// console.log(calculateWatchTime([100, 3800, "90" ]));
 
- // ---- Done ---- //
+//  // ---- Done ---- //
